66e462d7c36710d762773e0819f5d8015584a138
uson <usonkamylov0@gmail.com>
2023-02-10 19:47:50 -0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        StringBuilder word=new StringBuilder();
+        int[] num={1,2,3,4,5};
+        for (int s:num) {
+            word.append(s);
+
+        }
+        System.out.println(word);
+
+    }
+}
